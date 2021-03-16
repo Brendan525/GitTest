33a30479c11052e762c7b6647d65deebd2e07201
Brendan Burch <brendanburch@quickenloans.com>
2021-03-16 14:43:24 -0400

    Correction

--- GitTest/Program.cs
+++ GitTest/Program.cs
@@ -6,7 +6,7 @@ namespace GitTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("TESTING Feature B");
+            Console.WriteLine("TESTING Feature A");
         }
     }
 }
